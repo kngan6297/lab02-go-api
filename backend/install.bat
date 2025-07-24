a2765680a45588c7273e01324c87be8e8fe23718
kngan6297 <kngan6297@gmail.com>
2025-07-25 06:10:22 +0700

    update

--- backend/install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Installing backend dependencies...
-go mod tidy
-echo Backend dependencies installed successfully!
-echo.
-echo Don't forget to update the database password in config.env
-echo To start the backend, run: go run main.go
-pause 
